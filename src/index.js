144167a17601465c3d43716b61a95b0fd13dbc80
Ryan Davis <rdavis0020@gmail.com>
2023-12-09 14:46:03 -0500

    Lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', function() {
+  const taskForm = document.getElementById('taskForm');
+  const taskInput = document.getElementById('taskInput');
+  const taskList = document.getElementById('taskList');
+
+  taskForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const taskText = taskInput.value;
+
+    // Update the task list with the typed task
+    updateTaskList(taskText);
+  });
+
+  function updateTaskList(taskText) {
+    const listItem = document.createElement('li');
+    listItem.textContent = taskText;
+
+    // Add a delete button to each task
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', function() {
+      // Call a function to remove the task when the delete button is clicked
+      removeTask(listItem);
+    });
+
+    listItem.appendChild(deleteButton);
+    taskList.appendChild(listItem);
+
+    taskInput.value = '';
+  }
+
+  function removeTask(taskItem) {
+    // Remove the task item from the task list
+    taskList.removeChild(taskItem);
+  }
 });
+
